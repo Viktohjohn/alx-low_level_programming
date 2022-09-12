0fd8189bb4eb1438477d0e720b0d297948a82ab5
IfeanyiP <ezendukakuifeanyi@gmail.com>
2022-09-12 16:45:42 -0700

    8. onye ara

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,16 +8,6 @@
 
 int main(void)
 {
-	char *s = "and that piece is useful\" - Dpra Korpar, 2015-10-19\n";
-	long 1 = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
-	__asm__("syscall"
-			: "=a" (ret)
-			: "a" (syscall),
-			"D" (fd),
-			"S" (s),
-			"d" (1));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
