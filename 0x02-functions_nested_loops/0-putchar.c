1b322d8db27c11d3e5133219af9990f47411ae1e
IfeanyiP <ezendukakuifeanyi@gmail.com>
2022-09-13 13:11:09 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: program that prints -putchar followed by a new line
+ * Return: o
+ */
+int main(void)
+{
+	_putchar('\n');
+	return (0);
+}
