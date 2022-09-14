984b301dc2b8e7219974d65da7f11a35476934bf
IfeanyiP <ezendukakuifeanyi@gmail.com>
2022-09-13 21:36:59 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,10 +35,10 @@ int main(void)
 		}
 
 		printf("%lu%lu",half1, half2);
-		if (count 1= 98)
+		if (count != 98)
 			printf(", ");
 
-		fibi_half1 = fib2_half1;
+		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
