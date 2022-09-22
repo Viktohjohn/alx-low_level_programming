9827f26d53d1f7f5db0cd7cba82e9690c42a01ef
IfeanyiP <ezendukakuifeanyi@gmail.com>
2022-09-22 16:26:16 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,18 +12,28 @@
 
 char *leet(char *str)
 {
-	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-	while (str[indx1])
+	int i;
+	int j;
+	int k;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
 		{
-			if (str[indx1] == leet[indx2] ||
-			str[indx1] - 32 == leet[indx2])
-			str[indx1] = indx2 + '0';
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
 		}
-		
-		indx1++;
+		i++;
 	}
-	return (str);
+	return (s);
 }
