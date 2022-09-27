ae492359e17eba02dd9f53a32d15b848f68d837e
IfeanyiP <ezendukakuifeanyi@gmail.com>
2022-09-26 18:03:15 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * Description - A function that fills memory with a constant byte
  * *@s: the function accepts an input saved into s
  * *@b: the funtion accepts an input saved into b
- * *@n: the function acceptsan input saved int n
+ * *@n: the function accepts an input saved into n
  * Return: a char as success
  */
 char *_memset(char *s, char b, unsigned int n)
