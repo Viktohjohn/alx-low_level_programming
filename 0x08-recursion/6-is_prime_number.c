8cae6263660a92feb43af62f3a4b228edaa94ce5
IfeanyiP <ezendukakuifeanyi@gmail.com>
2022-09-28 20:31:23 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,14 +13,14 @@ int is_prime(unsigned int n, unsigned int c)
 		if (n == c)
 			return (1);
 		else
-			return(0);
+			return (0);
 	}
 	return (0 + is_prime(n, c + 1));
 }
 /**
- * is_primes - detects if an input number is a prine
+ * is_prime_number - detects if an input number is a prime number.
  * @n: input number.
- * Return: 1 if n is a prime number. 0 if n is not a prime number'
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
 int is_prime_number(int n)
 {
