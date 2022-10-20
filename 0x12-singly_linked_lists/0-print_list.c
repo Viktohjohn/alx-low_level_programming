e046bf9fbb916bbf9275cfb216412135ad06c29d
Ifeanyi Ezendukaku <111011436+IfeanyiP@users.noreply.github.com>
2022-10-20 21:33:50 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  * @h: list_t list
  * Return: number of nodes
  */
-size_t print_liat(const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t nodeCounter = 0;
 
@@ -14,8 +14,8 @@ size_t print_liat(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		else 
-			printf("[%d] %\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 		nodeCounter++;
